d7253045cd681ff104a70151fd79fedb04a4323b
godouuu <godoh107@gmail.com>
2024-03-01 12:35:02 -0600

    add flip_wipe texture2D, add transitionshader script for flip wipe texture, add page flip animation and switched it out of fade animation

--- Birthday-2024-Project/Scripts/ScreenManagement/ScreenManager.gd
+++ Birthday-2024-Project/Scripts/ScreenManagement/ScreenManager.gd
@@ -2,14 +2,18 @@ class_name ScreenManager
 extends Node
 
 @export var popupRoot : CanvasLayer
+@export var fade_duration: float = 1.5
 
 var lastScreen : String
 var _screenStack : Array[Node]
+#var tween = get_tree().create_tween()
+#@onready var colorRect: ColorRect = $TransitionLayer/AnimationPlayer/ColorRect
 @onready var transition = $TransitionLayer/AnimationPlayer
+#@onready var shaderMaterial: ShaderMaterial = colorRect.material
 
 func GoToScreen(screen : PackedScene, data : Dictionary, doTransition: bool):
 	if(doTransition):
-		transition.play("fade_out")
+		transition.play("page_flip")
 		await transition.animation_finished
 	var newScreen = screen.instantiate()
 	var screenLogic : ScreenLogic = newScreen as ScreenLogic
@@ -23,9 +27,9 @@ func GoToScreen(screen : PackedScene, data : Dictionary, doTransition: bool):
 	popupRoot.add_child(newScreen)
 	_screenStack.push_back(newScreen)
 	screenLogic.ScreenEnter.emit()
-	if(doTransition): 
-		transition.play("fade_in")
-		await transition.animation_finished
+	#if(doTransition): 
+		#transition.play("page_flip")
+		#await transition.animation_finished
 
 func IsTopScreen(screen : ScreenLogic) -> bool:
 	var topScreen : ScreenLogic = _screenStack.back() as ScreenLogic
@@ -51,7 +55,12 @@ func ShowConfirmationPopup(title : String, body : String, confirm : String = "Co
 	popupParameters[ConfirmationPopupController.CANCEL_KEY] = cancel
 	GoToScreen(load("res://MainScenes/confirmation_popup.tscn"), popupParameters, false)
 
-func _ready():
+func _ready():	
+	#shaderMaterial.set_shader_param("invert", true)
+	#shaderMaterial.set_shader_param("progress", 0)
+	#tween.tween_method(_set_progress, 0, 1, fade_duration)
+	#tween.tween_property(colorRect, "visible", "hide", fade_duration)
+	#tween.play()
 	GoToScreen(load("res://MainScenes/splash_screen.tscn"), {}, true)
 
 func _closeTopScreen():
@@ -60,3 +69,15 @@ func _closeTopScreen():
 	screenLogic.ScreenExit.emit()
 	lastScreen = oldScreen.name
 	oldScreen.queue_free()
+
+#func wipeTransition():
+		#colorRect.show()
+		#shaderMaterial.set_shader_param("invert", false)
+		#shaderMaterial.set_shader_param("progress", 0)
+		#tween.tween_method(_set_progress, 0, 1, fade_duration)
+		#tween.play()
+		#await tween.finished
+	#
+#func _set_progress(progress: float) -> void:
+	#shaderMaterial.set_shader_param("progress", progress)
+	
